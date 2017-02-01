33f72eba2a9c22e3e432f64b18f541f8077f2f41
Raphael A. Goyran <raphael@joinrebase.com>
2017-02-01 13:43:47 -0800

    Use SERVICE instead of HOSTNAME

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from multiprocessing import current_process
 from redis import StrictRedis, ConnectionPool
 from rq import Worker, Queue, Connection
 
-current_process().name = environ['HOSTNAME']
+current_process().name = environ['SERVICE']
 rsyslog.setup(log_level = environ['LOG_LEVEL'])
 LOGGER = logging.getLogger()
 
@@ -15,7 +15,7 @@ try:
     pool = ConnectionPool(host='redis', max_connections=1)
 
     with Connection(StrictRedis(connection_pool = pool)):
-        worker = Worker(environ['HOSTNAME'])
+        worker = Worker(environ['SERVICE'])
         worker.work(logging_level = environ['LOG_LEVEL'])
 except Exception as exc:
     LOGGER.exception('Unhandled exception!')
